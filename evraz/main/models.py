f3c41753233a372dad2f78b80f532bc2638d0d96
toly <tolytrader@gmail.com>
2023-02-18 12:10:59 +0300

    add signals

--- evraz/main/models.py
+++ evraz/main/models.py
@@ -7,3 +7,11 @@ class DataItem(models.Model):
     offset = models.BigIntegerField()
     key = models.CharField(max_length=1024)
     value = models.FloatField(null=True)
+
+
+class Signal(models.Model):
+
+    name = models.CharField(max_length=1024)
+    formula = models.TextField()
+    min_alert_value = models.FloatField(null=True, blank=True)
+    max_alert_value = models.FloatField(null=True, blank=True)
